e00bb71491bd4322bcc70e0b195e9d2798620b08
Spu7Nix <spu7nix@outlook.com>
2021-09-25 01:15:01 +0200

    update highlighting

--- prism-spwn.js
+++ prism-spwn.js
@@ -1,11 +1,12 @@
 Prism.languages.spwn = Prism.languages.extend("clike", {
   keyword:
-    /\b(?:else|for|if|return|error|extract|in|let|type|import|impl|break|\=>|\->|continue|switch|case|null|sync)\b/,
+    /\b(?:else|for|if|return|error|extract|in|let|type|import|impl|break|\=>|\->|continue|switch|case|null|sync|throw|while)\b/,
   builtin: /\b(?:null|trigger|obj|\$|self)\b/,
   boolean: /\b(?:true|false)\b/,
-  operator: /(==|!=|<=|>=|<|>|&&|\|\||!|=|\+\=|\-\=|\*\=|\/\=|\+|\-|\*|\/|%|\^|\.\.)/,
+  operator:
+    /(==|!=|<=|>=|<|>|&&|\|\||!|=|\+\=|\-\=|\*\=|\/\=|\+|\-|\*|\/|%|\^|\.\.|\-\-|\+\+|\/\%|\/\%=|\^=|as|has|<=>)/,
   number: /(?:\b\d+(\.\d+)?\b)|(\b([0-9]+|\?)[gbci]\b)/,
-  string: /\"[^\n\r\"]*\"/,
+  string: /[a-z]?"(?:\\.|[^\\"])*"|'(?:\\.|[^\\'])*'/,
   tag: /@([a-zA-Z_][a-zA-Z0-9_]*)/,
 })
 delete Prism.languages.spwn["class-name"]
